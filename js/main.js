ab718291e9101f4daf5668d68677e98f7110a21a
renan tomazini <renantomazini@gmail.com>
2022-07-01 07:04:13 -0300

    Aula 4

--- js/main.js
+++ js/main.js
@@ -17,14 +17,33 @@ function tocaSom(idAudio){
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let contador = 0;
+/*let contador = 0;
 
 while(contador < listaDeTeclas.length){
     const tecla = listaDeTeclas[contador]
     const instrumento = tecla.classList[1];
-    console.log(instrumento);
+    const idAudio = `#som_${instrumento}`
     tecla.onclick = function (){
-        tocaSom('#som_' + instrumento);
+        tocaSom(idAudio);
     };
     contador++;
+}*/
+
+for(let contador =0; contador < listaDeTeclas.length; contador++){
+    const tecla = listaDeTeclas[contador]
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`
+    tecla.onclick = function (){
+        tocaSom(idAudio);
+    };
+
+    tecla.onkeydown = function (evento){
+        if(evento.code === "Space" || evento.code =="Enter"){
+            tecla.classList.add('ativa');
+        }
+        
+    }
+    tecla.onkeyup = function (){
+        tecla.classList.remove('ativa');
+    }
 }
