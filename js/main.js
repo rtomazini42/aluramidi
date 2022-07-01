adfac376f61d04dfb079f7969779a61a1d6bf430
renan tomazini <renantomazini@gmail.com>
2022-07-01 05:37:40 -0300

    Aula 3

--- js/main.js
+++ js/main.js
@@ -2,11 +2,29 @@
 
 
 
-function tocaSomPom(){
+/*function tocaSomPom(){
     document.querySelector('#som_tecla_pom').play();
     console.log("Tocando");
+}
+*/
+
+function tocaSom(idAudio){
+    document.querySelector(idAudio).play();
 }
 
 
 
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+let contador = 0;
+
+while(contador < listaDeTeclas.length){
+    const tecla = listaDeTeclas[contador]
+    const instrumento = tecla.classList[1];
+    console.log(instrumento);
+    tecla.onclick = function (){
+        tocaSom('#som_' + instrumento);
+    };
+    contador++;
+}
